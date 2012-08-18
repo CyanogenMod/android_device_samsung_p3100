bbc57414375457acc7c8168c3117231cafe1089a
codeworkx <daniel.hillenbrand@codeworkx.de>
2012-08-18 23:54:07 +0200

    large mdpi device
    Change-Id: I806c3a3e258cbf56824c67f7661e230ffeda86d4

--- p31xx-common.mk
+++ p31xx-common.mk
@@ -23,9 +23,9 @@ $(call inherit-product, device/common/gps/gps_us_supl.mk)
 
 LOCAL_PATH := device/samsung/p3100
 
-# This device is hdpi.
-PRODUCT_AAPT_CONFIG := normal hdpi
-PRODUCT_AAPT_PREF_CONFIG := hdpi
+# This device is mdpi.
+PRODUCT_AAPT_CONFIG := large mdpi
+PRODUCT_AAPT_PREF_CONFIG := mdpi
 PRODUCT_LOCALES += hdpi
 
 # Init files
