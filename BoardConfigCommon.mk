5ec4959b534174fbdcfd82c58c54ef65d3d5ffc8
Vojtech Bocek <vbocek@gmail.com>
2015-03-01 22:37:11 +0100

    Use LZMA compression for recovery's ramdisk
    Change-Id: I5a27136c973d6641264249a7fb209848456bb283

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -116,5 +116,8 @@ TARGET_RECOVERY_FSTAB := device/samsung/p3100/rootdir/fstab.espresso
 RECOVERY_FSTAB_VERSION := 2
 BOARD_HAS_DOWNLOAD_MODE := true
 
+BOARD_CUSTOM_BOOTIMG_MK := device/samsung/p3100/custombootimg.mk
+TARGET_NOT_USE_GZIP_RECOVERY_RAMDISK := true
+
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/p31xx/BoardConfigVendor.mk
