8376ded50706d20c67d241c6f24ce661eab3b8e0
Ziyan <jaraidaniel@gmail.com>
2015-09-12 13:32:38 +0200

    espresso-common: commonize bluetooth configuration (2/3)
    Change-Id: Id23d0e90c01bddcb837859e959dad64254244989

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -20,9 +20,5 @@
 
 -include device/samsung/espresso-common/BoardConfigCommon.mk
 
-# Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/samsung/p3100/bluetooth
-BOARD_BLUEDROID_VENDOR_CONF := device/samsung/p3100/bluetooth/vnd_espresso.txt
-
 # Use the non-open-source parts, if they're present
 -include vendor/samsung/p31xx/BoardConfigVendor.mk
